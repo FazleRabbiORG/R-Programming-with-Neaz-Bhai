8530efc436b56ab62df8bfa4169441731bee93e2
Fazle Rabbi <24.fazlerabbi@gmail.com>
2022-11-07 19:03:10 +0600

    change brance name

--- lecture 2.R
+++ lecture 2.R
@@ -1,5 +1,19 @@
 #--------------#vector ----------------
 x<- c(1,2,3,4)
 
+
 typeof(x)
 class(x)
+
+#create Sequence
+x<- seq(from=1,to= 10,by=2)
+x2<- seq(from=1,to= 10,length.out = 5)
+x3 <- seq(along.with =3)
+x
+x2
+x3
+
+#Repeat on 
+
+x<- rep(2,4)
+x
